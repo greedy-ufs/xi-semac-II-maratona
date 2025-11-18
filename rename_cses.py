5047c0e730a31fb58008a4006d66d7a9f8b9c7e9
Daniel Rezende <danielrezende2@academico.ufs.br>
2025-11-17 21:26:12 -0300

    feat: add input and output files for question A and update renaming script

--- rename_cses.py
+++ rename_cses.py
@@ -1,15 +1,23 @@
 from pathlib import Path
 
-FOLDER_PATH = Path(__file__).parent / "B/output"
+QUESTION_CODE = "A"
 
-for file_path in FOLDER_PATH.iterdir():
-    if file_path.is_file():
-        file_stem = file_path.stem
-        new_name = f"B_{file_stem}"  # no extension
-        new_path = file_path.parent / new_name
-        counter = 1
-        while new_path.exists():
-            new_path = file_path.parent / f"{new_name}_{counter}"
-            counter += 1
-        file_path.rename(new_path)
-        print(f"Renamed: {file_path.name} -> {new_path.name}")
+FOLDER_INPUT_PATH = Path(__file__).parent / f"{QUESTION_CODE}/input"
+FOLDER_OUTPUT_PATH = Path(__file__).parent / f"{QUESTION_CODE}/output"
+
+def rename_prefix(folder_path: Path)->None:
+    for file_path in folder_path.iterdir():
+        if file_path.is_file():
+            file_stem = file_path.stem
+            new_name = f"{QUESTION_CODE}_{file_stem}"  # no extension
+            new_path = file_path.parent / new_name
+            counter = 1
+            while new_path.exists():
+                new_path = file_path.parent / f"{new_name}_{counter}"
+                counter += 1
+            file_path.rename(new_path)
+            print(f"Renamed: {file_path.name} -> {new_path.name}")
+
+
+rename_prefix(FOLDER_INPUT_PATH)
+rename_prefix(FOLDER_OUTPUT_PATH)
